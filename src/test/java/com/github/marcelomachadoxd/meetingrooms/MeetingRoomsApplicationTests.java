3d2138d67ebddbb0de29c2d8fb9e846efe5e55c7
MarceloMachadoxD <mastermanson@gmail.com>
2021-04-11 18:49:12 -0300

    Initial commit

---
+++ src/test/java/com/github/marcelomachadoxd/meetingrooms/MeetingRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelomachadoxd.meetingrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
