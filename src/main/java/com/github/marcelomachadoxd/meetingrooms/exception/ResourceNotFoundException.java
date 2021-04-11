777f3a037c57931e0e9a70f312aef3674482ef4e
MarceloMachadoxD <mastermanson@gmail.com>
2021-04-11 19:57:48 -0300

    Create Exception Classes

---
+++ src/main/java/com/github/marcelomachadoxd/meetingrooms/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.github.marcelomachadoxd.meetingrooms.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception{
+
+    private static final long SerialVersionUID= 1L;
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+
+
+}
